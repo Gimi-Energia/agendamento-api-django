5aaacfd28ee6ae6d0b1b6ad476de89ee5c21c559
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-08 11:20:18 -0300

    feat: field 'image' in Sala

--- agenda_me/salas/models.py
+++ agenda_me/salas/models.py
@@ -11,6 +11,7 @@ class Sala(models.Model):
 
     id = models.AutoField(primary_key=True) 
     empresa = models.CharField(choices=EMPRESA_CHOICES, max_length=100, verbose_name="Empresa")
+    image = models.ImageField(upload_to='uploads/sala/img', null=True, verbose_name='Imagem')
     name = models.CharField(max_length=255, blank=False, verbose_name="Nome")
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
