28e0bf5ba7a7523174dc5264c9bdd0c717dff7a3
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-15 16:56:10 -0300

    fix: release-tasks.sh wrong file path

--- release-tasks.sh
+++ release-tasks.sh
@@ -3,5 +3,5 @@ python agenda_me/manage.py migrate
 echo 'Migrations completed.'
 
 echo 'Loading initial data for database...'
-python manage.py loaddata fixtures
+python agenda_me/manage.py loaddata fixtures
 echo 'Initial data loaded.'
