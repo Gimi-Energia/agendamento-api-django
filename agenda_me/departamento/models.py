28c9e75fbd7c11ae82fb9de9e120430b2dcd3767
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-15 11:27:41 -0300

    refactor: add field 'company' to departments

--- agenda_me/departamento/models.py
+++ agenda_me/departamento/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from agenda_me.utils import EMPRESAS
+
 class Department(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False, verbose_name="nome")
+    company = models.CharField(max_length=255, default=EMPRESAS.GIMI, choices=EMPRESAS.CHOICES, blank=False, verbose_name="empresa")
 
     def __str__(self):
         return f'{self.name}'
