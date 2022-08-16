8fd72ca26bad96b18d8aaed115199e207465ba4c
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-16 12:03:51 -0300

    refactor: 'creator_email' field is now a foreign key of Email model

--- agenda_me/agenda/models/agenda_base.py
+++ agenda_me/agenda/models/agenda_base.py
@@ -1,16 +1,19 @@
+from functools import reduce
+import operator
 import time
 from django.db import models
 from unidecode import unidecode
 
 from salas.models import Sala
 from departamento.models import Department
+from emails.models import Email
 
 class AgendaBase(models.Model):
     titulo = models.CharField(max_length=255, blank=False, unique=False, verbose_name="Título")
     sala = models.ForeignKey(Sala, on_delete=models.CASCADE, blank=True, null=True)
     code = models.CharField(max_length=255, editable=False, blank=False, null=True, verbose_name="Código de segurança")
     created_by = models.CharField(max_length=100, null=False, blank=False, verbose_name="Criado por")
-    creator_email = models.EmailField(null=False, blank=False, verbose_name="Email de quem criou")
+    creator_email = models.ForeignKey(Email, on_delete=models.SET_NULL, null=True, blank=False, verbose_name="Email de quem criou")
     creator_department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, verbose_name="Departamento de quem criou")
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
@@ -48,6 +51,7 @@ class AgendaBase(models.Model):
             .filter(sala_id=sala.id))           
             )
         return agendamentos
+
 
     class Meta:
         abstract = True
