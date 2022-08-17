4e483dce10bd89a7fc8c4ceb554f1fa2f359b5ea
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-17 09:50:04 -0300

    refactor: Department 'company' field is nullable

--- agenda_me/departamento/models.py
+++ agenda_me/departamento/models.py
@@ -4,7 +4,7 @@ from agenda_me.utils import EMPRESAS
 
 class Department(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False, verbose_name="nome")
-    company = models.CharField(max_length=255, default=EMPRESAS.GIMI, choices=EMPRESAS.CHOICES, blank=False, verbose_name="empresa")
+    company = models.CharField(max_length=255, default=EMPRESAS.GIMI, choices=EMPRESAS.CHOICES, null=True, blank=False, verbose_name="empresa")
 
     def __str__(self):
         return f'{self.name}'
